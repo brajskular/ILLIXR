c79f1c8c2e43ceb7f7a6e11b3c4e8753841ffb2c
Mark Van Moer <mvanmoer@illinois.edu>
2023-07-10 18:15:27 -0500

    Removed explict "[<PLUGINNAME>]" from log messsages for derived plugins.
    plugin.name is used to register each plugin logger in the global spdlog
    registry. This will appear in the default log format pattern, or can be
    accessed by the %n format.

--- timewarp_gl/plugin.cpp
+++ timewarp_gl/plugin.cpp
@@ -564,11 +564,11 @@ public:
 
         if (log_count > LOG_PERIOD) {
             const double time_since_render_ms_d = duration2double<std::milli>(time_since_render);
-	    spdlog::get(name)->debug("[TIMEWARP] Time since render: {} ms", time_since_render_ms_d);
+	    spdlog::get(name)->debug("Time since render: {} ms", time_since_render_ms_d);
         }
 
         if (time_since_render > display_params::period) {
-	   spdlog::get(name)->warn("[TIMEWARP] Stale frame!");
+	   spdlog::get(name)->warn("Stale frame!");
         }
 #endif
         // Call Hologram
@@ -619,11 +619,11 @@ public:
             const time_point time_next_swap    = GetNextSwapTimeEstimate();
             const double     timewarp_estimate = duration2double<std::milli>(time_next_swap - time_last_swap);
 
-	    spdlog::get(name)->debug("[TIMEWARP] Swap time: {} ms", time_swap);
-	    spdlog::get(name)->debug("[TIMEWARP] Motion-to-display latency: {} ms", latency_mtd);
-	    spdlog::get(name)->debug("[TIMEWARP] Prediction-to-display latency: {} ms", latency_ptd);
-	    spdlog::get(name)->debug("[TIMEWARP] Render-to-display latency: {} ms", latency_rtd);
-	    spdlog::get(name)->debug("[TIMEWARP] Next swap in: {} ms in the future", timewarp_estimate);
+	    spdlog::get(name)->debug("Swap time: {} ms", time_swap);
+	    spdlog::get(name)->debug("Motion-to-display latency: {} ms", latency_mtd);
+	    spdlog::get(name)->debug("Prediction-to-display latency: {} ms", latency_ptd);
+	    spdlog::get(name)->debug("Render-to-display latency: {} ms", latency_rtd);
+	    spdlog::get(name)->debug("Next swap in: {} ms in the future", timewarp_estimate);
         }
 #endif
 
